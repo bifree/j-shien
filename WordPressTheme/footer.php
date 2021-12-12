c83616a6f7a7c8e85d622a16967b7798262285a3
bifree <bifree2bfree@gmail.com>
2021-12-12 16:22:17 +0900

    header/footer作成開始

---
+++ WordPressTheme/footer.php
@@ -0,0 +1,16 @@
+<footer>
+	<div class="l-footer">
+		<div class="footer-menu">
+			<ul>
+				<li><a href="">運営会社</a></li>
+				<li><a href="">プライバシーポリシー</a></li>
+				<li><a href="">反社会的勢力に対する基本方針はこちら</a></li>
+			</ul>
+		</div>
+		<div class="copyright">
+			&copy; 2010-2021 日本企業支援センター株式会社
+		</div>
+	</div>
+</footer>
+</body>
+</html>
---
+++ WordPressTheme/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>会社設立後のお助け隊｜日本企業支援センター</title>
+</head>
+<body>
+	<header>
+		<div class="l-header">
+			<div class="logo">
+				<img src="" alt="">
+			</div>
+			<div class="header-menu">
+				<nav>
+					<ul>
+						<li><a href="">はじめての方へ</a></li>
+						<li><a href="">電子申請キャンペーン</a></li>
+						<li><a href="">お客様の声</a></li>
+						<li><a href="">よくある質問</a></li>
+						<li><a href="">運営会社</a></li>
+					</ul>
+				</nav>
+			</div>
+			<div class="header-contact">
+				<div class="tel">
+					<p><span>0120-955-761</span></p>
+					<p>受付時間：10:00〜16:30(平日)</p>
+				</div>
+				<div class="apply-form">
+					<p>電子申請キャンペーン</p>
+					<p class="bigger">お申込み</p>
+				</div>
+			</div>
+		</div>
+	</header>
