f6748309b325508e4a18423c3a5294698b4cf0a3
aku424tt <61079024+aku424tt@users.noreply.github.com>
2021-12-26 18:41:01 +0900

    pc用の運営会社が完了しました。

---
+++ WordPressTheme/page-company.php
@@ -0,0 +1,137 @@
+<?php get_header(); ?>
+<main class="company">
+    <div class="com-fv">
+        <h1 class="_page-title _flex"><span><img src="<?php echo get_template_directory_uri(); ?>/assets/img/company-top-icon.png" alt=""></span>運営会社</h1>
+    </div>
+    <div class="page-info">
+        <div class="_inner">
+            <p class="pan-kuzu"><span>ホーム</span> ＞ <span>電子申請キャンペーン</span></p>
+        </div>
+    </div>
+    <section class="info">
+        <div class="company-inner info-wrap _flex">
+            <div class="info__company">
+                <table>
+                    <tr>
+                        <td>会社名</td>
+                        <td>日本企業支援センター株式会社</td>
+                    </tr>
+                    <tr>
+                        <td>所在地</td>
+                        <td>
+                            【本社】
+                            <br> 〒170-0005
+                            <br> 東京都豊島区南大塚3-43-12 
+                            <br> 高原ビル5階 
+                            <br><br> 【郡山オフィス】
+                            <br> 〒963-8004 
+                            <br> 福島県郡山市中町9-9
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>TEL/FAX</td>
+                        <td>
+                            03-6820-0560（代表）/
+                            <br> 03-5956-3677 (FAX)
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>URL</td>
+                        <td><a href="https://www.j-shien.co.jp">https://www.j-shien.co.jp</a></td>
+                    </tr>
+                    <tr>
+                        <td>設立日</td>
+                        <td>2009年1月14日</td>
+                    </tr>
+                    <tr>
+                        <td>資本金</td>
+                        <td>1,000万円</td>
+                    </tr>
+                    <tr>
+                        <td>代表取締役</td>
+                        <td>丸山　隆之</td>
+                    </tr>
+                    <tr>
+                        <td>事業内容</td>
+                        <td>
+                            ・会社設立支援事業 
+                            <br><span> ・中小企業に向けた会計事務所のマッチング事業</span>
+                            <br>・その他BtoB事業
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="info__map">
+                <div class="info__map-wrap">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3465.3901477730487!2d139.72687091283484!3d35.728646245785484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188d74513d66d5%3A0x1cb6c15c5f6ddd5!2z44CSMTcwLTAwMDUg5p2x5Lqs6YO96LGK5bO25Yy65Y2X5aSn5aGa77yT5LiB55uu77yU77yT4oiS77yR77ySIOmrmOWOn-ODk-ODqyA16ZqO!5e0!3m2!1sja!2sjp!4v1640495329172!5m2!1sja!2sjp" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+                <div class="info__map-privacy">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/company-pmark.png" alt="">
+                    <p>プライバシーマーク取得企業です</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="site">
+        <div class="company-inner">
+            <h2 class="site-title">運営サイト</h2>
+            <div class="site-inner  _flex">
+                <div class="site__text-wrap">
+                    <p class="site__text">
+                        税理士総合サイトT-SHIEN
+                    <br><a href="https://www.taxaccounting-shien.com/">https://www.taxaccounting-shien.com/</a> 
+                    <br><br> 税理士探しをサポートする日本最大級の税理士総合サイト。
+                    <br> 4万件以上のマッチング実績。税理士登録1000事務所以上。
+                    <br> 日本トレンドリサーチによる調査で2冠達成。
+                    <br> 自分に合った税理士が見つかるマッチングサイトNo.1/
+                    <br> 税理士情報充実度No.●
+                    </p>
+                </div>
+                <div class="site__img">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/company-tshien.png" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="media">
+        <div class="company-inner">
+            <h2 class="media-title">受賞・メディア掲載実績</h2>
+            <ul class="media-lists _flex">
+                <li class="media-lists__item">
+                    <div class="media-lists__item__img-wrap">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/company-list01.png" alt="">
+                    </div>
+                    <p>「ベストベンチャー100」に選ばれました</p>
+                </li>
+                <li class="media-lists__item">
+                    <div class="media-lists__item__img-wrap">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/company-list02.png" alt="">
+                    </div>
+                    <p>「注目企業50」に選ばれました</p>
+                </li>
+                <li class="media-lists__item">
+                    <div class="media-lists__item__img-wrap">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/company-list03.png" alt="">
+                    </div>
+                    <p>東京都高齢者職域開拓モデル事業に認定されました</p>
+                </li>
+                <li class="media-lists__item">
+                    <div class="media-lists__item__img-wrap">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/company-list04.png" alt="">
+                    </div>
+                    <p>テレビ番組「賢者の選択」に紹介されました</p>
+                </li>
+                <li class="media-lists__item">
+                    <div class="media-lists__item__img-wrap">
+                        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/company-list05.png" alt="">
+                    </div>
+                    <p>経営者通に紹介されました</p>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <?php get_template_part('template/appeal');?>
+    <?php get_template_part('template/apply-block');?>
+</main>
+<?php get_footer(); ?>
