07786354fa54b9fcf4640a633fd79a65d88dc0a3
aku424tt <61079024+aku424tt@users.noreply.github.com>
2021-12-28 08:43:43 +0900

    voiceページの初期設定完了

---
+++ WordPressTheme/page-voice.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+<main class="campaign manga">
+    <div class="com-fv">
+        <h1 class="_page-title _flex"><span><img src="<?php echo get_template_directory_uri(); ?>/assets/img/voice-top-icon.svg" alt=""></span>お客様の声</h1>
+    </div>
+    <div class="page-info">
+        <div class="_inner">
+            <p class="pan-kuzu"><span>ホーム</span> ＞ <span>お客様の声</span></p>
+        </div>
+    </div>
+</main>
+<?php get_footer(); ?>
