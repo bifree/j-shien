aa5d58ae6af90e8acef732def6c4f4bdaff133a6
bifree <bifree2bfree@gmail.com>
2021-12-30 00:27:17 +0900

    Top responsive

--- WordPressTheme/template/customer-voice.php
+++ WordPressTheme/template/customer-voice.php
@@ -39,43 +39,50 @@
 					</div>
 				</div>
 			</div>
-			<div class="voice-box">
-				<div class="voice-box_content">
-					<img src="https://todokede.sakura.ne.jp/ne2/wp-content/uploads/2021/12/voice_pict015.jpg" alt="">
-					<div class="name">
-						<p>株式会社HSP CONSULTING</p>
-						<p>代表取締役　フッセン・セイヤド・</p>
-						<p>ソルタン・ナイヤル 様</p>
+
+			<!-- スマホでは非表示 -->
+
+			<div class="sp-none">
+				<div class="voice-box">
+					<div class="voice-box_content">
+						<img src="https://todokede.sakura.ne.jp/ne2/wp-content/uploads/2021/12/voice_pict015.jpg" alt="">
+						<div class="name">
+							<p>株式会社HSP CONSULTING</p>
+							<p>代表取締役　フッセン・セイヤド・</p>
+							<p>ソルタン・ナイヤル 様</p>
+						</div>
+						<div class="text">
+							願ったり叶ったりの税理士でした
+						</div>
 					</div>
-					<div class="text">
-						願ったり叶ったりの税理士でした
+				</div>
+				<div class="voice-box">
+					<div class="voice-box_content">
+						<img src="https://todokede.sakura.ne.jp/ne2/wp-content/uploads/2021/12/voice_pict08.jpg" alt="">
+						<div class="name">
+							<p>Vision Industries 株式会社</p>
+							<p>代表取締役 石山　浩三 様</p>
+						</div>
+						<div class="text">
+							迷わずお願いしました。結果は大満足です
+						</div>
+					</div>
+				</div>
+				<div class="voice-box">
+					<div class="voice-box_content">
+						<img src="https://todokede.sakura.ne.jp/ne2/wp-content/uploads/2021/12/voice_pict09.jpg" alt="">
+						<div class="name">
+							<p>合同会社三重投資家倶楽部</p>
+							<p>代表社員 村林　守 様</p>
+						</div>
+						<div class="text">
+							はじめはどんな税理士を紹介されるのかと思い、躊躇しました
+						</div>
 					</div>
 				</div>
 			</div>
-			<div class="voice-box">
-				<div class="voice-box_content">
-					<img src="https://todokede.sakura.ne.jp/ne2/wp-content/uploads/2021/12/voice_pict08.jpg" alt="">
-					<div class="name">
-						<p>Vision Industries 株式会社</p>
-						<p>代表取締役 石山　浩三 様</p>
-					</div>
-					<div class="text">
-						迷わずお願いしました。結果は大満足です
-					</div>
-				</div>
-			</div>
-			<div class="voice-box">
-				<div class="voice-box_content">
-					<img src="https://todokede.sakura.ne.jp/ne2/wp-content/uploads/2021/12/voice_pict09.jpg" alt="">
-					<div class="name">
-						<p>合同会社三重投資家倶楽部</p>
-						<p>代表社員 村林　守 様</p>
-					</div>
-					<div class="text">
-						はじめはどんな税理士を紹介されるのかと思い、躊躇しました
-					</div>
-				</div>
-			</div>
+
+
 		</div>
 		<div class="btn mt-4">
 			<a href="">お客様の声の詳細はこちら</a>
