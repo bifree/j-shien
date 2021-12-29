d30e568ccd125849dae96dc0ffec1a7ab451afc9
bifree <bifree2bfree@gmail.com>
2021-12-29 20:00:47 +0900

    appealレスポンシブ

--- WordPressTheme/footer.php
+++ WordPressTheme/footer.php
@@ -2,9 +2,9 @@
 	<div class="l-footer">
 		<div class="footer-menu">
 			<ul>
-				<li><a href="">運営会社</a></li>
-				<li><a href="">プライバシーポリシー</a></li>
-				<li><a href="">反社会的勢力に対する基本方針はこちら</a></li>
+				<li><a href="<?php echo esc_url( home_url( '/company' ) ); ?>">運営会社</a></li>
+				<li><a href="https://www.todokede-shien.com/policy/index.html">プライバシーポリシー</a></li>
+				<li><a href="https://j-shien.co.jp/antisocial.pdf">反社会的勢力に対する基本方針はこちら</a></li>
 			</ul>
 		</div>
 		<div class="copyright">
