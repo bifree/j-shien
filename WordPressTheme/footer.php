a9674d93b4c65ed3755b578566c78af1962b97ff
bifree <bifree2bfree@gmail.com>
2022-01-05 17:16:46 +0900

    プライバシポリシーリンク変更

--- WordPressTheme/footer.php
+++ WordPressTheme/footer.php
@@ -3,7 +3,7 @@
 		<div class="footer-menu">
 			<ul>
 				<li><a href="<?php echo esc_url( home_url( '/company' ) ); ?>">運営会社</a></li>
-				<li><a href="https://www.todokede-shien.com/policy/index.html">プライバシーポリシー</a></li>
+				<li><a href="https://www.j-shien.co.jp/privacy.pdf">プライバシーポリシー</a></li>
 				<li><a href="https://j-shien.co.jp/antisocial.pdf">反社会的勢力に対する基本方針はこちら</a></li>
 			</ul>
 		</div>
