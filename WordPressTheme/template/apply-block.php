86d97715934155db16322c22b8e226384cc5f882
bifree <bifree2bfree@gmail.com>
2022-01-05 18:04:24 +0900

    面倒な手続きを税理士にお任せimg化

--- WordPressTheme/template/apply-block.php
+++ WordPressTheme/template/apply-block.php
@@ -2,7 +2,7 @@
 	<div class="apply-block-inner">
 		<div class="block-content_1">
 			<p class="mr-5 sp-center">設立３カ月以内の法人様限定</p>
-			<p class="sp-center"><span class="slash">\</span><span class="bigger">面倒な手続きを税理士にお任せ！</span><span class="slash">/</span></p>
+			<p class="sp-center"><img src="https://todokede.sakura.ne.jp/ne2/wp-content/uploads/2022/01/260ecb42ad265feca28bfa75467af62f.png" alt=""></p>
 		</div>
 		<?php get_template_part('template/yellow-btn'); ?>
 		<div class="free-dial">
